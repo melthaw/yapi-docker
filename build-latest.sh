544176a01870c5516758925a15b830e8d1cf1534
melthaw <melthaw@gmail.com>
2020-07-06 18:16:10 +0800

    fixed dockerfile

--- build-latest.sh
+++ build-latest.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
-docker build -f docker/Dockerfile docker -t clouthinkin/yapi:latest
+docker build -f docker/Dockerfile-latest docker -t clouthinkin/yapi:latest
 docker push clouthinkin/yapi:latest
